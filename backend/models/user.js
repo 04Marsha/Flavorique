2a8d6ea4f34e414e8f7d8d7510318e6dc3e77fb4
04Marsha <161226959+04Marsha@users.noreply.github.com>
2025-06-23 18:50:40 +0530

    commit 4

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,10 +2,21 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: async function (value) {
+        const count = await this.constructor.countDocuments({ email: value });
+        return count === 0;
+      },
+      message: "Email already exists",
+    },
+  },
+  password: { type: String, required: true },
 });
 
 userSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -106,7 +106,4 @@ export class PostsService {
     });
   }
 
-  hello() {
-    console.log("hello");
-  }
 }
