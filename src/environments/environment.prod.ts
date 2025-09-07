2ed780fa4f85082fdd3f483c2a375091dcde1fa3
04Marsha <161226959+04Marsha@users.noreply.github.com>
2025-09-07 11:10:01 +0530

    updates-11

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl:
-    'http://flavor-env.eba-4tphm8hm.eu-north-1.elasticbeanstalk.com/api',
+    'https://flavorique.onrender.com/api',
 };
